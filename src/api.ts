820b91873f1772a8eb068ad3913c79aedd784fcf
Lucas Bruno <lucasbrunoferreira@gmail.com>
2024-04-07 20:13:23 -0300

    fix: remove the empty lines

--- src/api.ts
+++ src/api.ts
@@ -8,18 +8,14 @@ app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://lucas:lucas@localhost:5432/cccat16");
 	try {
 		const id = crypto.randomUUID();
-
 		const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
 		if (!acc) {
-
 			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
 				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
 					if (validate(req.body.cpf)) {
 						if (req.body.isDriver) {
 							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
 								await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
 								const obj = {
 									accountId: id
 								};
@@ -30,7 +26,6 @@ app.post("/signup", async function (req, res) {
 							}
 						} else {
 							await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
 							const obj = {
 								accountId: id
 							};
@@ -44,12 +39,10 @@ app.post("/signup", async function (req, res) {
 					// invalid email
 					result = -2;
 				}
-
 			} else {
 				// invalid name
 				result = -3;
 			}
-
 		} else {
 			// already exists
 			result = -4;
